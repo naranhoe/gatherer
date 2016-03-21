d0b417c2f178e97b97582fff8a8d9685d219fb58
Jorge Naranjo <j.naranjojr@gmail.com>
2016-03-21 18:19:11 -0400

    Test made to check if a particular task is on schedule or not

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,12 +1,36 @@
 class Project
-  attr_accessor :tasks
+  attr_accessor :tasks, :due_date
 
   def initialize
     @tasks = []
   end
 
   def done?
-    tasks.empty?
+    tasks.reject(&:complete?).empty?
+  end
+
+  def total_size
+    tasks.sum(&:size)
+  end
+
+  def remaining_size
+    tasks.reject(&:complete?).sum(&:size)
+  end
+
+  def completed_velocity
+    tasks.sum(&:points_towards_velocity)
+  end
+
+  def current_rate
+    completed_velocity * 1.0 / 21
+  end
+
+  def projected_days_remaining
+    remaining_size / current_rate
+  end
+
+  def on_schedule?
+    (Date.today + projected_days_remaining) <= due_date
   end
 
 end
